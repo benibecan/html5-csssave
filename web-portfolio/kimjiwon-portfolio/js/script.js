3499b7d2f536f82c15e72ceaa3c3788ad942019e
benibecan <benibecan@naver.com>
2023-09-15 17:57:58 +0900

    0915

--- web-portfolio/kimjiwon-portfolio/js/script.js
+++ web-portfolio/kimjiwon-portfolio/js/script.js
@@ -2,3 +2,20 @@
 //     var a = $('ul.box li').css('width');
 //     $('ul.box li').css('height', a);
 // });
+
+//온클릭 이벤트
+$(document).ready(function () {
+    //팝업 모달
+
+    $('.hi').click(function () {
+        $('#himodal').show();
+    });
+});
+//팝업 Close 기능
+function close_pop(flag) {
+    $('#himodal').hide();
+}
+
+// $(document).on('click', '.hi', function () {
+//     alert('동작확인');
+// });
